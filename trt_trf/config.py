044de2ea1a270424782ee5ee8800d94ece8207b0
trisongz <tri@songzcorp.com>
2021-12-28 16:57:52 -0600

    Update config.py

--- trt_trf/config.py
+++ trt_trf/config.py
@@ -17,16 +17,19 @@ class TRTWorkspaceConfig:
     t5_encoder_workspace_mb: int = Env.to_int('T5_ENCODER_TRT_WORKSPACE_MB', 16384)
     t5_decoder_workspace_mb: int = Env.to_int('T5_DECODER_TRT_WORKSPACE_MB', 24576)
 
+    @classmethod
     def set_workspace_gb(cls, gb: int = 16):
         """ Sets all the workspaces to gb * 1024"""
         cls.default_trt_workspace_mb = gb * 1024
         cls.t5_encoder_workspace_mb = gb * 1024
         cls.t5_decoder_workspace_mb = gb * 1024
     
+    @classmethod
     def set_default_workspace_gb(cls, gb: int = 16):
         """ Sets all the default workspaces to gb * 1024"""
         cls.default_trt_workspace_mb = gb * 1024
 
+    @classmethod
     def set_t5_workspace_gb(cls, gb: int = 16):
         """ Sets all the t5 workspaces to gb * 1024"""
         cls.t5_encoder_workspace_mb = gb * 1024
